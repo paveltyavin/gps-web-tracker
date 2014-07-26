dcadc046cfaaa5302ea115711c92f7a669db8d8e
Pavel Tyavin <pavel.tyavin@msk.action.ru>
2014-07-27 01:02:28 +0400

    #

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function (grunt) {
 
+  var revision = grunt.option('revision') || '12345';
+  var now = new Date;
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     requirejs: {
@@ -7,10 +9,13 @@ module.exports = function (grunt) {
         options: {
           baseUrl: "./browser",
           mainConfigFile: "./browser/require.config.js",
-          out: "build/app-min.js",
+          out: "build/app-"+revision+".js",
 //          optimize:'none',
           name: "../bower_components/almond/almond",
-          include: ['app']
+          include: [
+            'app',
+            '../bower_components/raven-js/dist/raven'
+          ]
         }
       }
     },
@@ -22,6 +27,13 @@ module.exports = function (grunt) {
       }
     },
     processhtml: {
+      options: {
+        data: {
+          revision: revision,
+          modified: now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate(),
+          now: now
+        }
+      },
       dist: {
         files: {
           'build/index.html': 'browser/index.html'
